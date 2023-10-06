92fbff73de14ce6a478572c52e262518c0ca2bbb
Cédric Phung <cedric3011@outlook.fr>
2023-10-06 14:44:23 +0200

    adding logo

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,41 +1,51 @@
+import Image from "next/image";
+
 const Nav = () => {
   return (
     <>
-{/* Bootstrap navbar */}
-<div class="navbar-menu d-flex justify-content-end">
-    <nav class="navbar navbar-expand-lg">
-      <div class="container-fluid d-flex justify-content-end">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"    aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-              <div class="d-flex justify-content-end">
-                <li class="nav-item">
-                <a class="nav-link" href="#">Accueil</a>
-              </li>
+<div className="d-flex justify-content-between">
+   {/* Logo */}
+      <Image
+      src="/images/logo/logo.png"
+      height="150"
+      width="150"
+      className="p-2"
+      />
+
+    {/* Bootstrap navbar */}
+      <nav class="navbar navbar-expand-lg navbar-menu p-5">
+        <div class="container-fluid d-flex justify-content-end">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"    aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav">
+                <div class="d-flex justify-content-end">
+                  <li class="nav-item">
+                  <a class="nav-link" href="#">Accueil</a>
+                </li>
+                </div>
+                <div class="d-flex justify-content-end">
+                  <li class="nav-item">
+                  <a class="nav-link" href="#">Galerie</a>
+                </li>
+                </div>
+                <div class="d-flex justify-content-end">
+                  <li class="nav-item">
+                  <a class="nav-link" href="#">Tarifs</a>
+                </li>
+                </div>
+                <div class="d-flex justify-content-end">
+                  <li class="nav-item">
+                  <a class="nav-link" href="#">Contact</a>
+                </li>
+                </div>
+              </ul>
               </div>
-              <div class="d-flex justify-content-end">
-                <li class="nav-item">
-                <a class="nav-link" href="#">Galerie</a>
-              </li>
-              </div>
-              <div class="d-flex justify-content-end">
-                <li class="nav-item">
-                <a class="nav-link" href="#">Tarifs</a>
-              </li>
-              </div>
-              <div class="d-flex justify-content-end">
-                <li class="nav-item">
-                <a class="nav-link" href="#">Contact</a>
-              </li>
-              </div>
-            </ul>
-            </div>
-        </div>
-    </nav>
-</div>
-    </>
+          </div>
+      </nav>
+  </div>
+  </>
   )
 }
 
