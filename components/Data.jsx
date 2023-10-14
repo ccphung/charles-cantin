1b80028690440b217385584a6292ab2af4c63952
Cédric Phung <cedric3011@outlook.fr>
2023-10-14 11:33:39 +0200

    changing the path of the images

--- components/Data.jsx
+++ components/Data.jsx
@@ -4,7 +4,7 @@ const Data =
             id:1,
             title:"bébé",
             category:"bébé",
-            src: 'public/images/bebes/jonathan-borba-CgWTqYxHEkg-unsplash.jpg',
+            img: 'public/images/bebes/jonathan-borba-CgWTqYxHEkg-unsplash.png',
             alt: "baby picture"
         },
         {
