62a8722edd6a0986d09d2b904ccd714b433bdc3f
Cédric Phung <cedric3011@outlook.fr>
2023-10-16 14:32:35 +0200

    modifying fetch function

--- app/page.jsx
+++ app/page.jsx
@@ -3,9 +3,15 @@ import Link from "next/link"
 
 
 const getHomeData = async () =>  {
-  const request = await fetch(`${process.env.API_URL}/api/homes?populate=*`)
-  const response = await request.json()
+  const reqOptions = {
+      headers: {
+        Authorization: `bearer ${process.env.API_TOKEN}`,
+      },
+      cache: 'no-store'
+    };
 
+  const request = await fetch("http://127.0.0.1:1337/api/homes?populate=*", reqOptions)
+  const response = await request.json()
   return await response
 }
 
