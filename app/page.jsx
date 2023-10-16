3970abf71bd9b10f900e4d7a5a7409a68bdede85
Cédric Phung <cedric3011@outlook.fr>
2023-10-16 13:45:00 +0200

    adding await response

--- app/page.jsx
+++ app/page.jsx
@@ -12,7 +12,7 @@ const getHomeData = async () =>  {
   const request = await fetch(`http://127.0.0.1:1337/api/homes?populate=*`, reqOptions)
   const response = await request.json()
 
-  return response
+  return await response
 }
 
 const Home = async() => {
--- app/tarifs/page.jsx
+++ app/tarifs/page.jsx
@@ -1,53 +1,53 @@
-// import './tarif.css'
-// import Link from "next/link"
-// import Container from "react-bootstrap/Container"
-// import Col  from "react-bootstrap/Col"
-// import Row from 'react-bootstrap/Row';
-// import Image from "react-bootstrap/Image"
+import './tarif.css'
+import Link from "next/link"
+import Container from "react-bootstrap/Container"
+import Col  from "react-bootstrap/Col"
+import Row from 'react-bootstrap/Row';
+import Image from "react-bootstrap/Image"
 
-// const getPricingData = async () =>  {
-//     const reqOptions = {
-//         headers: {
-//           Authorization: `Bearer ${process.env.API_TOKEN}`,
-//         },
-//         cache: 'no-store'
-//       };
+const getPricingData = async () =>  {
+    const reqOptions = {
+        headers: {
+          Authorization: `Bearer ${process.env.API_TOKEN}`,
+        },
+        cache: 'no-store'
+      };
 
-//     const request = await fetch(`http://127.0.0.1:1337/api/pricings?populate=*`, reqOptions)
-//     const response = await request.json()
-//     return response
-// }
+    const request = await fetch(`http://127.0.0.1:1337/api/pricings?populate=*`, reqOptions)
+    const response = await request.json()
+    return await response
+}
 
-// const page = async () => {
-//     const pricing = await getPricingData()
-//     const prices = await pricing.data
-//   return (
-//   <>
-//   {console.log(prices[0].attributes.Image.data[0].attributes.url)}
-//         <main>
-//             <div className="title-container">
-//                 <h2>{prices[0].attributes.Title}</h2>
-//             </div>
+const page = async () => {
+    const pricing = await getPricingData()
+    const prices = await pricing.data
+  return (
+  <>
+  {console.log(prices[0].attributes.Image.data[0].attributes.url)}
+        <main>
+            <div className="title-container">
+                <h2>{prices[0].attributes.Title}</h2>
+            </div>
 
-//             <Container>
-//                 <Row>
-//                     {prices.map(price => (
-//                     <Col xs={12} md={6} lg={4}>
-//                           <div class="card m-3">
-//                           <Image class="card-img-top" src={`http://127.0.0.1:1337${price.attributes.Image.data[0].attributes.url}`} alt="Card image cap"/>
-//                           <div class="card-body">
-//                               <h5 class="card-title">{price.attributes.Option}<span className="colored-text">{price.attributes.Price}</span> </h5>
-//                               <p class="card-text">{price.attributes.Description}</p>
-//                               <Link href="\contact"><button className="btn">{price.attributes.Button}</button></Link>
-//                           </div>
-//                       </div>
-//                     </Col>
-//                     ))}
-//                 </Row>
-//             </Container>
-//         </main>
-//     </>
-//   )
-// }
+            <Container>
+                <Row>
+                    {prices.map(price => (
+                    <Col xs={12} md={6} lg={4}>
+                          <div class="card m-3">
+                          <Image class="card-img-top" src={`http://127.0.0.1:1337${price.attributes.Image.data[0].attributes.url}`} alt="Card image cap"/>
+                          <div class="card-body">
+                              <h5 class="card-title">{price.attributes.Option}<span className="colored-text">{price.attributes.Price}</span> </h5>
+                              <p class="card-text">{price.attributes.Description}</p>
+                              <Link href="\contact"><button className="btn">{price.attributes.Button}</button></Link>
+                          </div>
+                      </div>
+                    </Col>
+                    ))}
+                </Row>
+            </Container>
+        </main>
+    </>
+  )
+}
 
-// export default page
+export default page
