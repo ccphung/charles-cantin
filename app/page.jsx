7fa48e9086b627cee3cbe355465a18c3380bea50
Cédric Phung <cedric3011@outlook.fr>
2023-10-16 19:54:38 +0200

    modifying fetch function on tarif page

--- app/page.jsx
+++ app/page.jsx
@@ -1,28 +1,28 @@
-import Image from "next/image"
+"use client"
+import Image from "next/image";
 import Link from "next/link"
+import useSWR from "swr";
+import axios from 'axios';
 
-const getHomeData = async () =>  {
-  const reqOptions = {
-      headers: {
-        Authorization: `bearer ${process.env.API_TOKEN}`,
-      },
-      cache: 'no-store'
-    };
+const fetcher = (url) => axios.get(url).then((res) => res.data);
 
-  const request = await fetch(`http://127.0.0.1:1337/api/homes?populate=*`, reqOptions)
-  const response = await request.json()
-  return await response
-}
 
-const Home = async() => {
-  const homes = await getHomeData()
+const Home = () => {
+  const {data, error} = useSWR('http://127.0.0.1:1337/api/homes?populate=*', fetcher);
+
+  if (error) return <div>Error loading data...</div>;
+  if (!data) return <div>Loading...</div>;
+
+  console.log('data', data.data)
   return (
+
   <>
   <section className="home-page">
     {/* Background */}
+
     <div>
     <Image
-        src={`http://127.0.0.1:1337${homes.data[0].attributes.background.data.attributes.url}`}
+        src={`http://127.0.0.1:1337${data.data[0].attributes.background.data.attributes.url}`}
         layout="fill"
         objectFit="cover"
         quality={100}
@@ -33,25 +33,25 @@ const Home = async() => {
     </div>
 
     {/* Title */}
-    <h1 className="title text-center">{homes.data[0].attributes.title}</h1>
+    <h1 className="title text-center">{data.data[0].attributes.title}</h1>
 
     {/* Sub title  */}
     <div className="spin-wrapper">
-      <span className="static-text text-center">{homes.data[0].attributes.subtitle}</span> 
+      <span className="static-text text-center">{data.data[0].attributes.subtitle}</span> 
       <span className="dynamic-text"></span>
     </div>
 
     {/* Socials */}
     <div className="socials text-center">
           <Image
-            src={`http://127.0.0.1:1337${homes.data[0].attributes.socials.data[0].attributes.url}`}
+            src={`http://127.0.0.1:1337${data.data[0].attributes.socials.data[0].attributes.url}`}
             width={30}
             height={30}
             color="white"
             alt="facebook logo"
           />
           <Image
-            src={`http://127.0.0.1:1337${homes.data[0].attributes.socials.data[1].attributes.url}`}
+            src={`http://127.0.0.1:1337${data.data[0].attributes.socials.data[1].attributes.url}`}
             width={30}
             height={30}
             color="white"
--- app/tarifs/page.jsx
+++ app/tarifs/page.jsx
@@ -1,44 +1,41 @@
+"use client"
 import './tarif.css'
 import Link from "next/link"
 import Container from "react-bootstrap/Container"
 import Col  from "react-bootstrap/Col"
 import Row from 'react-bootstrap/Row';
-import Image from "react-bootstrap/Image"
+import Image from "react-bootstrap/Image";
+import useSWR from "swr";
+import axios from 'axios';
 
-const getPricingData = async () =>  {
-    const reqOptions = {
-        headers: {
-          Authorization: `bearer ${process.env.API_TOKEN}`,
-        },
-        cache: 'no-store'
-      };
+const fetcher = (url) => axios.get(url).then((res) => res.data);
 
-    const request = await fetch(`http://127.0.0.1:1337/api/pricings?populate=*`, reqOptions)
-    const response = await request.json()
-    return await response
-}
+const page = () => {
+  const {data, error} = useSWR('http://127.0.0.1:1337/api/pricings?populate=*', fetcher);
 
-const page = async () => {
-    const pricing = await getPricingData()
-    const prices = await pricing.data
+  if (error) return <div>Error loading data...</div>;
+  if (!data) return <div>Loading...</div>;
+
+    const datas = data.data
+
   return (
   <>
-  {console.log(prices[0].attributes.Image.data[0].attributes.url)}
+  {console.log(datas[0].attributes.Image.data[0].attributes.url)}
         <main>
             <div className="title-container">
-                <h2>{prices[0].attributes.Title}</h2>
+                <h2>{datas[0].attributes.Title}</h2>
             </div>
 
             <Container>
                 <Row>
-                    {prices.map(price => (
+                    {datas.map(data => (
                     <Col xs={12} md={6} lg={4}>
                           <div class="card m-3">
-                          <Image class="card-img-top" src={`http://127.0.0.1:1337${price.attributes.Image.data[0].attributes.url}`} alt="Card image cap"/>
+                          <Image class="card-img-top" src={`http://127.0.0.1:1337${data.attributes.Image.data[0].attributes.url}`} alt="Card image cap"/>
                           <div class="card-body">
-                              <h5 class="card-title">{price.attributes.Option}<span className="colored-text">{price.attributes.Price}</span> </h5>
-                              <p class="card-text">{price.attributes.Description}</p>
-                              <Link href="\contact"><button className="btn">{price.attributes.Button}</button></Link>
+                              <h5 class="card-title">{data.attributes.Option}<span className="colored-text">{data.attributes.Price}</span> </h5>
+                              <p class="card-text">{data.attributes.Description}</p>
+                              <Link href="\contact"><button className="btn">{data.attributes.Button}</button></Link>
                           </div>
                       </div>
                     </Col>
