1664537621e0f2db935660444b0ba4146a1275ea
Cédric Phung <cedric3011@outlook.fr>
2023-10-16 13:01:04 +0200

    adding fetch function

--- app/tarifs/page.jsx
+++ app/tarifs/page.jsx
@@ -6,6 +6,18 @@ import Row from 'react-bootstrap/Row';
 import Image from "react-bootstrap/Image"
 import getPricingData from '../features/getPricingData';
 
+const getPricingData = async () =>  {
+    const reqOptions = {
+        headers: {
+          Authorization: `Bearer ${process.env.API_TOKEN}`,
+        },
+        cache: 'no-store'
+      };
+
+    const request = await fetch(`http://127.0.0.1:1337/api/pricings?populate=*`, reqOptions)
+    const response = await request.json()
+    return response
+}
 
 const page = async () => {
     const pricing = await getPricingData()
