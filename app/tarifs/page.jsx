883aea28c145fbfeb4a768470bc8b31067311eb2
Cédric Phung <cedric3011@outlook.fr>
2024-01-10 16:45:15 +0100

    modify token

--- app/tarifs/page.jsx
+++ app/tarifs/page.jsx
@@ -1,14 +1,15 @@
-// Importez les modules nécessaires
-import './tarif.css';
-import Link from "next/link";
-import Container from "react-bootstrap/Container";
-import Col from "react-bootstrap/Col";
+"use client"
+import './tarif.css'
+import Link from "next/link"
+import Container from "react-bootstrap/Container"
+import Col  from "react-bootstrap/Col"
 import Row from 'react-bootstrap/Row';
 import Image from "react-bootstrap/Image";
 import useSWR from "swr";
 import axios from 'axios';
 
-// Fonction pour récupérer les données depuis Strapi avec le token
+
+// Fetching the data from Strapi
 const fetcherWithToken = (url, token) => {
   const authHeader = {
     headers: {
@@ -19,49 +20,47 @@ const fetcherWithToken = (url, token) => {
   return axios.get(url, authHeader).then((res) => res.data);
 };
 
-const Page = () => {
+const page = () => {
   const accessToken = process.env.API_TOKEN;
-  console.log("AccessToken:", accessToken);
 
   const { data, error } = useSWR(
     'https://charles-cantin-strapi-01d205b7c2c1.herokuapp.com/api/pricings?populate=*',
     () => fetcherWithToken('https://charles-cantin-strapi-01d205b7c2c1.herokuapp.com/api/pricings?populate=*', accessToken)
   );
 
-  // Gestion des erreurs et du chargement
+
   if (error) return <div>Erreur de chargement...</div>;
   if (!data) return <div>Chargement...</div>;
 
-  // Récupérez les données
-  const datas = data.data;
+    const datas = data.data
 
   return (
-    <>
-      {/* Titre */}
-      <main>
+  <>
+  {/*Title*/}
+    <main>
         <div className="title-container">
-          <h2>{datas[0].attributes.Title}</h2>
+            <h2>{datas[0].attributes.Title}</h2>
         </div>
-        {/* Cartes */}
+  {/* Card */}
         <Container>
-          <Row>
-            {datas.map(data => (
-              <Col xs={12} md={6} lg={4} key={data.id}>
-                <div className="card m-3">
-                  <Image className="card-img-top" src={`https://charles-cantin-strapi-01d205b7c2c1.herokuapp.com${data.attributes.Image.data[0].attributes.url}`} alt="Card image cap" />
-                  <div className="card-body">
-                    <h5 className="card-title">{data.attributes.Option}<span className="colored-text">{data.attributes.Price}</span> </h5>
-                    <p className="card-text">{data.attributes.Description}</p>
-                    <Link href="\contact"><button className="btn">{data.attributes.Button}</button></Link>
+            <Row>
+                {datas.map(data => (
+                <Col xs={12} md={6} lg={4}>
+                      <div className="card m-3">
+                      <Image className="card-img-top" src={`https://charles-cantin-strapi-01d205b7c2c1.herokuapp.com${data.attributes.Image.data[0].attributes.url}`} alt="Card image cap"/>
+                      <div className="card-body">
+                          <h5 className="card-title">{data.attributes.Option}<span className="colored-text">{data.attributes.Price}</span> </h5>
+                          <p className="card-text">{data.attributes.Description}</p>
+                          <Link href="\contact"><button className="btn">{data.attributes.Button}</button></Link>
+                      </div>
                   </div>
-                </div>
-              </Col>
-            ))}
-          </Row>
+                </Col>
+                ))}
+            </Row>
         </Container>
-      </main>
-    </>
-  );
+    </main>
+  </>
+  )
 }
 
-export default Page;
+export default page
